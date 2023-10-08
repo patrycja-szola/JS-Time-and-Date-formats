df41049c4ff3c66788cb433801ccf8b1898b28f7
szyszka212 <szyszka212@gmail.com>
2023-10-08 15:13:50 +0200

    Adding moment() function in JS

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
 function sayHello(){
 // alert("Hello");
 
-let helloBox = document.querySelector("#hello");    // it chooses the element IN html with #hello ID
-helloBox.innerHTML = "Bonjour!";    // variable helloBox will changed in HTML to "Bonjour"
+let helloBox = document.querySelector("#hello");
+// it chooses the element IN html with #hello ID
+helloBox.innerHTML = "Bonjour!";
+// variable helloBox will changed in HTML to "Bonjour"
 
 }
-setTimeout(sayHello, 3000);  // ! it will run the function 1000 milisek (=1sek) after reload the page
+setTimeout(sayHello, 2000);
+// ! it will run the function 1000 milisek (=1sek) after reload the page
 
 function displayDate(){
     let date = document.querySelector("#date");
-    date.innerHTML = new Date();    // variable date will now show the current date
+    date.innerHTML = new Date();
+    // variable date will now show the current date
 }
 
-setInterval(displayDate, 1000);      // this function will show the date every second => we will have the digital clock with real-time
+setInterval(displayDate, 1000);
+// this function will show the date every second => we will have the digital clock with real-time
 // for minutes we should use 60000ms instead of 1000ms
+
+let todayElement = document.querySelector("#today");
+let today = moment().format("dddd Do MMMM, YYYY H:m")
+    todayElement.innerHTML = `Today is ${today}`;
+
+    let tomorrowElement = document.querySelector("#tomorrow");
+    let tomorrow = moment().add(1, "day").format("dddd Do, MMMM");
+    tomorrowElement.innerHTML = `Tomorrow will be ${tomorrow}`
+
+    let nextWeekElement = document.querySelector("#nextWeek");
+    let nextWeek = moment().add(7, "day").format("dddd Do, MMMM");
+    nextWeekElement.innerHTML= `In 7 days we will have ${nextWeek}`;
